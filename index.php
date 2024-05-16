91485e317e3edda488a18672bc671f448152974a
Rayan <rianetrabelsi@outlook.fr>
2024-05-16 23:32:52 +0200

    nouveau fichier : index.php 	nouveau fichier : signup/signup.css 	nouveau fichier : signup/signup.php 	nouveau fichier : signup/signup_process.php 	nouveau fichier : style.css

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Rhost</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form method="POST" action="login_process.php">
+        <input name="username" placeholder="username">
+        <input name="password" placeholder="password">
+        <input value="log in" type="submit">
+    </form>
+
+</body>
+</html>
